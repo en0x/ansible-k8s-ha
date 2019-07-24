f9ad89e06329a174bbcc41c256a6e30c5830fb6c
Pawel Kilian <pawel.kilian@slalom.com>
2019-07-23 21:14:54 -0500

    the first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vagrant
+
+- See [Vagrant.md](Vagrant.md)
+
+# Ansible
+
+# Kubernetes
+- Keepalived - one common IP for masters
+- haproxy to balance between nodes
+- 3 masters running
---
+++ vagrant_config.rb
@@ -0,0 +1,7 @@
+# Vagrant machine variables
+$cpus = 2
+$memory = 2048
+$os_image = "centos/7"
+$private_subnet = "172.16.35"
+$provider = "virtualbox"
+$ansible_playbook = "ansible/playbook.yml"
